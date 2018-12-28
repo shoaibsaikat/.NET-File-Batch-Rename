db35b92aeb39611589262633bbb3e7867a00718a
Mina Shoaib Rahman <shoaibsaikat@gmail.com>
2018-12-28 20:32:50 +0600

    Initial commit

---
+++ FileBatchRename/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows;
+using System.IO;
+
+namespace FileBatchRename
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnReplace_Click(object sender, RoutedEventArgs e)
+        {
+            if(tbDirectory.Text != string.Empty && tbOrigName.Text != string.Empty)
+            {
+                var files = Directory.GetFiles(tbDirectory.Text, tbOrigName.Text + "*");
+                String name = string.Empty;
+                if(tbReplaceName.Text != string.Empty)
+                {
+                    foreach (var item in files)
+                    {
+                        name = item.Replace(tbOrigName.Text, tbReplaceName.Text);
+                        File.Move(item, name);
+                    }
+                }
+            }
+        }
+    }
+}
